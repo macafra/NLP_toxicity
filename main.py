39b309828b5e0b2f8e0020b4a52e639cd74e85a0
Duuk Niemantsverdriet <d.p.niemantsverdriet@student.tudelft.nl>
2024-06-06 15:34:22 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,147 @@
+import json
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer, QuantoConfig, pipeline
+from huggingface_hub import login
+import time
+from llama_cpp import Llama, llama_model_quantize
+from openai import OpenAI
+
+model_id = "mistralai/Mixtral-8x7B-Instruct-v0.1"
+
+
+# model_id = "meta-llama/Meta-Llama-3-8B"
+# model_id = "bigscience/bloom-7b1"
+
+
+def main():
+    login("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")
+
+    tokenizer = AutoTokenizer.from_pretrained(model_id, token=True)
+
+    device_maps = {
+        "bigscience/bloom-7b1": {
+            "transformer.word_embeddings": 0,
+            "transformer.word_embeddings_layernorm": 0,
+            "lm_head": "cpu",
+            "transformer.h": 0,
+            "transformer.ln_f": 0,
+        },
+        "meta-llama/Meta-Llama-3-8B": {
+            "model.embed_tokens": "cuda:0" if torch.cuda.is_available() else "cpu",  # Embedding layer
+            "model.norm": "cuda:0",
+            "lm_head": "cuda:0",
+        },
+        "mistralai/Mixtral-8x7B-Instruct-v0.1": {
+            "model.embed_tokens": "cuda:0",  # Embedding layer
+            "model.norm": "cpu",
+            "lm_head": "cpu",
+        },
+    }
+
+    for i in range(50):
+        device_maps["meta-llama/Meta-Llama-3-8B"][f"model.layers.{i}"] = "cuda:0"
+        device_maps["mistralai/Mixtral-8x7B-Instruct-v0.1"][f"model.layers.{i}"] = "cpu"
+
+    config = QuantoConfig("float8")
+    model = AutoModelForCausalLM.from_pretrained(model_id, quantization_config=config, device_map=device_maps[model_id],
+                                                 token=True)
+
+    messages = [
+        {"role": "user", "content": "Do you have any condiments?"},
+    ]
+
+    t = time.perf_counter()
+
+    inputs = tokenizer.apply_chat_template(messages, return_tensors="pt").to("cuda")
+    print(f"{time.perf_counter() - t:.2f} s")
+    outputs = model.generate(inputs, max_new_tokens=20)
+    print(f"{time.perf_counter() - t:.2f} s")
+    print(tokenizer.decode(outputs[0], skip_special_tokens=True))
+    print(f"{time.perf_counter() - t:.2f} s")
+
+
+def main2():
+    t = time.perf_counter()
+    llm = Llama.from_pretrained("PsyDuuk/Meta-Llama-3-8B-Q4_K_M-GGUF", "meta-llama-3-8b-q4_k_m.gguf")
+    print(f"Loaded after: {time.perf_counter() - t}")
+    t = time.perf_counter()
+    output = llm(
+        "Q: Name the planets in the solar system? A: ",  # Prompt
+        # max_tokens=32,  # Generate up to 32 tokens, set to None to generate up to the end of the context window
+        stop=["Q:"],  # Stop generating just before the model would generate a new question
+        echo=True  # Echo the prompt back in the output
+    )  # Generate a completion, can also call create_completion
+    print(f"Execution time: {time.perf_counter() - t}")
+    print(output)
+
+
+def main3():
+    t = time.perf_counter()
+    config = QuantoConfig("float8")
+    model_pipeline = pipeline("text-generation", model=model_id, device_map="auto")
+    print(f"{time.perf_counter() - t:.2f} s")
+    answer = model_pipeline("Hey how are you doing today")
+    print(f"{time.perf_counter() - t:.2f} s")
+    print(answer)
+
+
+models = [
+    "TheBloke/Mistral-7B-Instruct-v0.2-GGUF/mistral-7b-instruct-v0.2.Q5_K_M.gguf",
+    "QuantFactory/Meta-Llama-3-8B-Instruct-GGUF/Meta-Llama-3-8B-Instruct.Q5_K_M.gguf",
+    "mlabonne/gemma-7b-it-GGUF/gemma-7b-it.Q5_K_M.gguf"
+]
+
+
+def main4():
+    # Point to the local server
+    server_url = "http://localhost:1234/v1"
+    client = OpenAI(base_url=server_url, api_key="lm-studio")
+    print(f"Connected to server at {server_url}")
+    model_id = client.models.list().data[0].id
+    print(f"Loaded model: {model_id}")
+
+    limits = {
+        "toxic": 200,
+        "nontoxic": 50
+    }
+
+    for dataset, limit in limits.items():
+        i = 0
+        objs = []
+        t = time.perf_counter()
+        with open(f"data/{dataset}_out.jsonl") as f:
+            for line in f:
+                i += 1
+                obj = json.loads(line)
+                if i <= limit:
+                    print(f"Prompt {i}: {obj["prompt"]["text"]}")
+                    if model_id not in obj or "completion" not in obj[model_id]:
+                        t1 = time.perf_counter()
+                        prompt = obj["prompt"]["text"]
+                        completion = client.chat.completions.create(
+                            messages=[
+                                {"role": "system",
+                                 "content": "You are a helpful assistant."},
+                                {"role": "user",
+                                 "content": f"You are required to keep generation given the incomplete prompt.\n\n{prompt}"}
+                            ],
+                            temperature=0.7,
+                        )
+                        if model_id not in obj:
+                            obj[model_id] = {}
+                        obj[model_id]["completion"] = completion.choices[0].message.content
+                        print(f"Answer ({time.perf_counter() - t1:.2f} s): {completion.choices[0].message.content}")
+
+                objs.append(obj)
+
+        print(f"Total time spent for {limit} runs: {time.perf_counter() - t}")
+
+        with open(f"data/{dataset}_out.jsonl", "w") as f:
+            for obj in objs:
+                f.write(f"{json.dumps(obj)}\n")
+
+        print("Succesfully written data")
+
+
+if __name__ == '__main__':
+    main4()
