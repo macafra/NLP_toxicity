75f27bc0a514885f608178ae235903e1a75241b1
Margot <mac.pauelsen@gmail.com>
2024-06-11 10:23:58 +0200

    added a script to investigate toxic responses

---
+++ extract_toxic_responses.py
@@ -0,0 +1,43 @@
+import json
+
+def get_column_names(file_path):
+    with open(file_path, 'r') as file:
+        # Read the first line to get the column names
+        first_line = file.readline()
+        first_data = json.loads(first_line)
+        column_names = list(first_data.keys())
+        print("Column Names:", column_names)
+
+
+
+def filter_texts_with_negative_scores(file_path):
+    with open(file_path, 'r') as file:
+        for line in file:
+            data = json.loads(line)
+            if data['toxicity_score'] < 0:
+                print(data['text'])
+
+# filter_texts_with_negative_scores('data/toxic_out.jsonl')
+# get_column_names('data/toxic_out.jsonl')
+
+def print_negative_scores(file_path):
+    # model = 'QuantFactory/Meta-Llama-3-8B-Instruct-GGUF/Meta-Llama-3-8B-Instruct.Q5_K_M.gguf'
+    # model = 'TheBloke/Mistral-7B-Instruct-v0.2-GGUF/mistral-7b-instruct-v0.2.Q5_K_M.gguf'
+    # model = 'mlabonne/gemma-7b-it-GGUF/gemma-7b-it.Q5_K_M.gguf'
+    with open(file_path, 'r') as file:
+        for line in file:
+            data = json.loads(line)
+            
+            # Check if 'a' key exists and it is a dictionary
+            if model in data and isinstance(data[model], dict):
+                scores = data[model].get('toxicity_score')  # Access the 'scores' key within the 'a' dictionary
+                if scores is not None and isinstance(scores, (int, float)) and scores < 0:
+                    reply = data[model].get('reply')  # Access the 'reply' key within the 'a' dictionary
+                    score = data[model].get('toxicity_score')  # Access the 'reply' key within the 'a' dictionary
+
+                    if reply is not None:
+                        print("Reply:", reply)
+                        print("Score:", score)
+
+
+print_negative_scores('data/nontoxic_out.jsonl')
